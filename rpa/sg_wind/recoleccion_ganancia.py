8d7606915407b5386eab9812c6e2b7dda51cdabc
John Piedrahita <jhon.piedrahita@outlook.com>
2025-07-06 23:40:50 -0500

    modificacion experimental

--- rpa/sg_wind/recoleccion_ganancia.py
+++ rpa/sg_wind/recoleccion_ganancia.py
@@ -13,9 +13,9 @@ def recolectar_ganancia():
 
     try:
         driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/nav/a[4]').click(); sleep(5)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[1]/button'))).click(); sleep(1)
-        mensaje = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'toast-container')]//div[1]")))
-        print(mensaje.text); sleep(5)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[1]/button'))).click(); sleep(1)
+#        mensaje = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'toast-container')]//div[1]")))
+#        print(mensaje.text); sleep(5)
     except Exception as e:
         print(e)
     finally:
@@ -24,10 +24,10 @@ def recolectar_ganancia():
 def login(num_celular, num_password):
 
     try:
-        celular = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div[1]/div[2]/div/form/div[1]/div/input'))); celular.send_keys(num_celular)
-        contrasena = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div[1]/div[2]/div/form/div[2]/input'))); contrasena.send_keys(num_password)
+        celular = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div[1]/div[2]/div/form/div[1]/div/input'))); celular.send_keys(num_celular)
+        contrasena = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div[1]/div[2]/div/form/div[2]/input'))); contrasena.send_keys(num_password)
         ingresar = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[1]/div[2]/div/form/button[1]')))
-        ingresar.click(); sleep(5)
+        ingresar.click(); sleep(10)
 
     except Exception as e:
         print(e)
@@ -42,7 +42,8 @@ def iniciar_proceso():
             global driver
             args = Options()
             args.add_argument("--headless=new")
-            driver = webdriver.Chrome(options=args)
+            #driver = webdriver.Chrome(options=args)
+            driver = webdriver.Chrome()
             driver.get('https://sg-wind.com/#/login'); sleep(5)
             
             if len(sys.argv[1]) == 10:
