f590b1ee1bfcedc0857c0aa5bef059ce15b25512
John Piedrahita <jhon.piedrahita@outlook.com>
2025-07-09 00:35:38 -0500

    Se ajusta condicional para notificacion de ganancia

--- rpa/sg_wind/recoleccion_ganancia.py
+++ rpa/sg_wind/recoleccion_ganancia.py
@@ -6,22 +6,54 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
+import time
 import sys
 
-def recolectar_ganancia():
+def verificar_nprogress_o_toast(driver, tiempo_total=2, intervalo=0.2):
+
+    inicio = time.time()
+    nprogress_detectado = False
+    toast_mensaje = None
+
+    while time.time() - inicio < tiempo_total:
+        try:
+            nprogress = driver.find_element(By.ID, "nprogress")
+            if nprogress.is_displayed():
+                nprogress_detectado = True
+        except NoSuchElementException:
+            pass
+
+        try:
+            toast = driver.find_element(By.XPATH, "//div[contains(@class, 'toast-container')]//div[1]")
+            if toast.is_displayed():
+                toast_mensaje = toast.text
+                break  
+        except NoSuchElementException:
+            pass
+
+        sleep(intervalo)
+
+    if nprogress_detectado:
+        print("Se ha recolectado la ganancia diaria.")
+    elif toast_mensaje:
+        print(toast_mensaje)
+    else:
+        print("No se detectó ninguna acción después de presionar el botón.")
+
+def recolectar_ganancia(driver):
 
     try:
         driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/nav/a[4]').click(); sleep(5)
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[1]/button'))).click(); sleep(1)
-#        mensaje = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'toast-container')]//div[1]")))
-#        print(mensaje.text); sleep(5)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[1]/button'))).click(); sleep(0.5)
+        verificar_nprogress_o_toast(driver)
     except Exception as e:
         print(e)
     finally:
         driver.quit()
 
-def login(num_celular, num_password):
+def login(driver, num_celular, num_password):
 
     try:
         celular = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div[1]/div[2]/div/form/div[1]/div/input'))); celular.send_keys(num_celular)
@@ -32,22 +64,20 @@ def login(num_celular, num_password):
     except Exception as e:
         print(e)
 
-    recolectar_ganancia()
+    recolectar_ganancia(driver)
 
 def iniciar_proceso():
 
     try:
         if len(sys.argv) == 3:
-            global args 
-            global driver
             args = Options()
             args.add_argument("--headless=new")
-            #driver = webdriver.Chrome(options=args)
-            driver = webdriver.Chrome()
+            driver = webdriver.Chrome(options=args)
+            #driver = webdriver.Chrome()
             driver.get('https://sg-wind.com/#/login'); sleep(5)
             
             if len(sys.argv[1]) == 10:
-                login(num_celular=sys.argv[1], num_password=sys.argv[2])
+                login(driver, num_celular=sys.argv[1], num_password=sys.argv[2])
             else:
                 print("Suministre un numero valido de celular")
         else:
